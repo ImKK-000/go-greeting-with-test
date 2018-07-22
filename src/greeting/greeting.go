a73110831ff93c07f7e96b07b2be4fec8931d75a
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-07-22 13:00:55 +0700

    [Created]: สร้างฟังก์ชัน makeStringWithPrefix สำหรับสร้างข้อความพร้อมกับข้อความด้านหน้า

--- src/greeting/greeting.go
+++ src/greeting/greeting.go
@@ -4,7 +4,11 @@ const PREFIX = "hello "
 
 func Message(msg string) string {
 	if len(msg) == 0 {
-		return PREFIX + "thailand"
+		return makeStringWithPrefix("thailand")
 	}
+	return makeStringWithPrefix(msg)
+}
+
+func makeStringWithPrefix(msg string) string {
 	return PREFIX + msg
 }
