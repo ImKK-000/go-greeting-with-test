42d6166649516bfe226073f2b4ebe02bd959b858
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-07-22 15:08:42 +0700

    [Edited]: ส่วนที่ฮาร์ดโค้ดไว้และทดสอบกรณีส่งข้อความเปล่า

--- src/api/handler.go
+++ src/api/handler.go
@@ -6,17 +6,24 @@ import (
 	"net/http"
 )
 
+type RequestMessage struct {
+	Message string `json:"message"`
+}
+
 type ResponseMessage struct {
 	Message string `json:"message"`
 }
 
 func HelloPost(responseWriter http.ResponseWriter, request *http.Request) {
+	var requestMessage RequestMessage
+	body := json.NewDecoder(request.Body)
+	body.Decode(&requestMessage)
 	greeting := greeting.Greeting{
 		Prefix:             "hello",
 		DefaultEmptyString: "thailand",
 	}
 	resposeMessage := ResponseMessage{
-		Message: greeting.Message("world"),
+		Message: greeting.Message(requestMessage.Message),
 	}
 	encoder := json.NewEncoder(responseWriter)
 	encoder.Encode(resposeMessage)
--- src/api/handler_test.go
+++ src/api/handler_test.go
@@ -9,6 +9,23 @@ import (
 	"testing"
 )
 
+func TestGreetingHandleFuncWithEmptyMessage(t *testing.T) {
+	expectedResult := `{"message":"hello thailand"}`
+	requestMessage := `{"message":""}`
+	request := httptest.NewRequest(http.MethodPost, "/greeting", strings.NewReader(requestMessage))
+	responseRecorder := httptest.NewRecorder()
+
+	HelloPost(responseRecorder, request)
+
+	response := responseRecorder.Result()
+	responseBody, _ := ioutil.ReadAll(response.Body)
+	actualResult := strings.TrimSpace(string(responseBody))
+
+	if expectedResult != actualResult {
+		t.Errorf("Expect %v but it got %v", expectedResult, actualResult)
+	}
+}
+
 func TestGreetingHandleFunc(t *testing.T) {
 	expectedResult := `{"message":"hello world"}`
 	requestMessage := `{"message":"world"}`
